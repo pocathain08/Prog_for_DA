7e45692ac3c2ee72dbf8b12445073ac718db9f11
Peadar O Cathain <peadarocathain@gmail.com>
2019-10-30 13:14:14 +0000

    GCD defined as a function

--- gcd.py
+++ gcd.py
@@ -1,11 +1,29 @@
 #This program calculates the GCD of two numbers, a and b. Peadar o Cathain 30/10/2019
 
-a = 50
-b = 20
+def gcd(a, b):
+  """ 
+  #triple " allows the addition of a line
+  Returns the Greatest Common Divisor of a and b.
+  """
+  if a < b:
+    a, b = b, a
+  #This swops the values to keep the greater on the left
+    
+  while b > 0:
+    a, b = b, a % b
 
-while b > 0:
-  a, b = b, a % b
-#LHS = NEw Values, RHS = Old Values
+  return a
 
-  print(b)
+print (gcd(50, 20))
+print (gcd(22, 143 ))
+
+#The following is the original prog that didn't work!
+#a = 50
+#b = 20
+
+#while b > 0:
+#  a, b = b, a % b
+#LHS = New Values, RHS = Old Values
+
+  #print(b)
 
