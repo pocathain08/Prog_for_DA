e4fb1f6f1220d9844ce65e65aa7f930e0859acd6
Peadar O Cathain <peadarocathain@gmail.com>
2019-10-30 11:47:03 +0000

    Modification 1

--- Tuesday.py
+++ Tuesday.py
@@ -5,9 +5,17 @@ import datetime
 today = datetime.datetime.today()
 dayofweek = today.weekday()
 
+print ("Let's check if it's Tuesday")
+
 if dayofweek == 1:
-    print("It's Tuesday!")
+  print("It's Tuesday!")
+elif dayofweek == 0:
+  print("It's not Tuesday.")
+  print("Luckily, it will be Tuesday tomorrow!")
+
 else:
-    print("Unofortunately, it's not Tuesday.")
+  print("Unfortunately, it's not Tuesday.")
+
+print ("Thanks for checking it it's Tuesday")
 
 
