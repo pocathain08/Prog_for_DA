64d8d17a10ec3abf9e49ac54d1f5b06475fe78e2
Peadar O Cathain <peadarocathain@gmail.com>
2019-10-30 12:53:46 +0000

    While loop incl Rng gen and f string

---
+++ squares.py
@@ -0,0 +1,11 @@
+#l = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for i in range (20):
+    #print ("i, i*i)
+    #print (i*i)
+    print(f"{i:2} {i*i:4}")
+    #f string : 2 is a spacing formating feature. Prints as a column
+print (list(range(20)))
+# prints the range 0 ...19 on one line as a row
+#SEE range generator and f strings
+#print ("That's the whole list!")
